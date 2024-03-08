d5d8d8919e9af56c6da72d69b97fec8cd9b4fc0e
Elan Ruusamäe <glen@pld-linux.org>
2024-03-08 15:17:19 +0200

    Add PublicList tests

---
+++ tests/test_lists.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from trakt.users import PublicList
+
+
+def test_public_list():
+    trakt_id = 1248149
+    l = PublicList.load(trakt_id)
+
+    assert isinstance(l, PublicList)
+    assert l.name == "MARVEL Cinematic Universe"
+    assert l.privacy == "public"
+    assert l.share_link == "https://trakt.tv/lists/1248149"
